9f372436502bbd67b12faeb0c5c93e3ead4c35dc
corgab <corbanigabriele@gmail.com>
2024-03-01 14:39:44 +0100

    ADD: esericizio completato

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const number = 100 //number
+
+console.log(number)
+
+// esercizio 2
+
+let PI = 3.14 //number (con const non è possibile cambiare)
+
+PI = 3.1416 
+
+console.log(PI)
+
+const radius = 8 //number
+
+const circle = radius * 2 * PI
+
+console.log(circle)
+
+// esercizio 3
+
+let name = "Carlo" //string
+
+name = "Marco"
+
+console.log(name)
+
+
